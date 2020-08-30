0daaedeca47b9b85ca3b664a96f18f87529576b2
MrPoudel <baalkissna2012@gmail.com>
2020-08-30 12:56:40 +0200

    added main but delete it immediately

--- main.cpp
+++ main.cpp
@@ -1,42 +1,48 @@
-#include <iostream>
-#include "Thread.hpp"
-#include <signal.h>
-#include "Fault.h"
 
+// Standard library includes
+#include <iostream>
+#include <signal.h>
+
+// Third party includes
+#include "mqtt/async_client.h"
+
+// User defined inlcudes
 #include "ClassA.hpp"
 #include "ClassB.hpp"
+#include "Fault.h"
+#include "Thread.hpp"
 
-using namespace    std;
+using namespace std;
 
 // Worker thread instances
 static A threadA("Workerthread A");
 static B threadB("Workerthread B");
 
-static void exitProgram(int32_t interruptSignal)
-{
-  std::cout <<"---------- Exiting Application ----------" << std::endl;
-  std::cout <<"---------- Exiting Application ----------" << std::endl;
+static void
+exitProgram(int32_t interruptSignal) {
+    std::cout << "---------- Exiting Application ----------" << std::endl;
+    std::cout << "---------- Exiting Application ----------" << std::endl;
 
-  threadA.ExitThread();
-  threadB.ExitThread();
+    threadA.ExitThread();
+    threadB.ExitThread();
 
-  exit(interruptSignal);
+    exit(interruptSignal);
 }
 
-int main(void)
-{    
+int
+main(void) {
     signal(SIGINT, exitProgram);
-    signal(SIGTERM, exitProgram);     
+    signal(SIGTERM, exitProgram);
 
-    std::cout <<"---------- Starting Application ----------" << std::endl;
-    std::cout <<"---------- Starting Application ----------" << std::endl;
+    std::cout << "---------- Starting Application ----------" << std::endl;
+    std::cout << "---------- Starting Application ----------" << std::endl;
 
     // Create worker threads
     threadA.CreateThread();
     threadB.CreateThread();
 
     threadA.registerObserver(&threadB);  // threadB is the observer of threadA.
-    
+
     // // Create message to send to worker thread 1
     // UserDataA* userData1 = new UserDataA();
     // userData1->msg = "Hello world";
@@ -44,25 +50,27 @@ int main(void)
     // DataMsg<UserDataA> *myData = new DataMsg<UserDataA>(userData1);
     // threadA.PostMsg(myData);
 
-    // // Give time for messages processing on worker threads
+    // Give time for messages processing on worker threads
     // this_thread::sleep_for(1s);
 
     // UserDataB* userData2 = new UserDataB();
-    // userData2->msg = "Goodbye world"; 
+    // userData2->msg = "Goodbye world";
     // userData2->evId = ClassBEventIds::TASKB_MESSAGE_PRINT_LOG;
     // DataMsg<UserDataB> *myData2 = new DataMsg<UserDataB>(userData2);
     // threadB.PostMsg(myData2);
 
     // Give time for messages processing on worker threads
-    this_thread::sleep_for(5s); // If you don't do delay then the thread loop will overright the 
-    //value
-    threadB.ExitTimer(); // By default every thread has one timer created
-    // Current we don't need timer for thread B, in future if we need we can activate it again.
-    
-    while(true); 
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000)); // Allow some delay before exiting timer
+
+    threadB.ExitTimer();  // By default every thread has one timer created
+    // Current we don't need timer for thread B, in future if we need we can
+    // activate it again.
+
+    while(true)
+        ;
 
     // This should be unreachable
-    ASSERT();   
+    ASSERT();
 
     return 0;
 }
